7acddc0b56fd9e4cdd063b26b411cef646a20f2f
Burniee <52307044+Burniee@users.noreply.github.com>
2022-05-28 13:39:45 -0700

    Removed extra togglestate

--- source/src/scripts/Timer.js
+++ source/src/scripts/Timer.js
@@ -142,32 +142,6 @@ function toggleState() {
     }
 }
 
-/** Toggle focus mode: remove task list component and only show the Pomodoro timer */
-/** The function is keep track of focus tasks and check if all the
- * tasks are complete.
-  */
-function toggleState() {
-    // elements -- popup button, task list div, pomodoro timer div, focus task
-    // const popUpBtn = document.getElementById('popup-button');
-    const taskListDiv = document.getElementById('task-list');
-    const pomoDiv = document.getElementById('pomodoro-timer');
-    const focusTask = document.getElementById('focus-task');
-    const button = document.getElementById('header-buttons');
-    // popUpBtn.classList.toggle('state');
-    taskListDiv.classList.toggle('state');
-    pomoDiv.classList.toggle('state');
-    focusTask.classList.toggle('state');
-    button.classList.toggle('state');
-    if (localStorage.getItem('state') === 'default') {
-        localStorage.setItem('state', 'focus');
-    } else {
-        localStorage.setItem('state', 'default');
-        const title = document.getElementById('select-focus');
-        if (title.innerHTML === 'All tasks complete!') {
-            title.innerHTML = '';
-        }
-    }
-}
 
 /**
  * Updates the transition on tab refocus to prevent transition desync.
