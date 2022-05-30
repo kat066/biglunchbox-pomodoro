5bf9289488d007012a9bc5aa7d7e4e53f7337379
zianlin <64982992+zianlin@users.noreply.github.com>
2022-05-29 18:17:31 -0700

    Updated HelpPopUp.js code to match most recent.

--- source/src/components/HelpPopUp.js
+++ source/src/components/HelpPopUp.js
@@ -5,6 +5,12 @@
  * (DOM), and adds the elements of the help popup window to the DOM.
  */
 class HelpPopUp extends HTMLElement {
+    //Opens the popup.
+    openPopUp() {
+        const wrapper = this.shadowRoot.getElementById('help-popup');
+        wrapper.style.display = 'block';
+    }
+    
     //Closes the popup.
     closePopUp() {
         const wrapper = this.shadowRoot.getElementById('help-popup');
@@ -21,6 +27,9 @@ class HelpPopUp extends HTMLElement {
         shadow.appendChild(templateContent.cloneNode(true));
 
         Object.defineProperties(this, {
+            _bindedOpen: {
+                value: this.openPopUp.bind(this),
+            },
             _bindedClose: {
                 value: this.closePopUp.bind(this),
             },
@@ -33,22 +42,30 @@ class HelpPopUp extends HTMLElement {
             return;
         }
 
-        const closeBtn = this.shadowRoot.getElementById('close-icon');
+        const shadow = this.shadowRoot;
+
+        const closeBtn = shadow.getElementById('close-icon');
         closeBtn.addEventListener('click', this._bindedClose);
+
+        this.addEventListener('openPopUp', this._bindedOpen);
     }
 
     //If node is connected, remove the close button's on-click listener.
     disconnectedCallback() {
-        const closeBtn = this.shadowRoot.getElementById('close-icon');
+        const shadow = this.shadowRoot;
+
+        const closeBtn = shadow.getElementById('close-icon');
         closeBtn.removeEventListener('click', this._bindedClose);
+
+        this.removeEventListener('openPopUp', this._bindedOpen);
     }
 }
 customElements.define('help-popup', HelpPopUp);
 
-window.addEventListener('DOMContentLoaded', () => {
-    const helpBtn = document.getElementById('help-button');
-    const helpPopUp = document.querySelector('help-popup');
+function init() {
+   const helpBtn = document.getElementById('help-button');
     helpBtn.addEventListener('click', () => {
+        const helpPopUp = document.querySelector('help-popup');
         const btnSound = new Audio('./icons/btnClick.mp3');
         btnSound.volume = 0.01 * parseInt(localStorage.getItem('volume'), 10);
         if (localStorage.getItem('clickState') === 'on') {
@@ -56,11 +73,17 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         // this makes sure any popup is closed before opening current popup
         const popups = Array.from(document.getElementsByClassName('popup'));
-        for (let i = 0; i < popups.length; i += 1) {
-            popups[i].closePopUp();
-        }
-        helpPopUp.shadowRoot.getElementById('help-popup').setAttribute('style', 'display:block');
+        popups.forEach((el) => {
+            el.closePopUp();
+        });
+        helpPopUp.dispatchEvent(new Event('openPopUp'));
     });
-});
+}
+
+if (document.readyState !== 'loading') {
+    init();
+} else {
+    window.addEventListener('DOMContentLoaded', init);
+}
 
 // module.exports = HelpPopUp;
